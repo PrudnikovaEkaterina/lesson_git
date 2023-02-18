247aea5f769cdf0029b2a7760d02e290e1ce2acd
Ekaterina Prudnikova <rvsn0913@mail.ru>
2023-02-18 15:19:40 +0300

    chanch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello lesson_1!");
+        System.out.println("Hello lesson_1!!!!");
     }
 
     public static void getInfo(){
